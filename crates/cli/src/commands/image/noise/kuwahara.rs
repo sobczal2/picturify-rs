914841eee5ffa4a652d0f8c1c4ee859f3b85eef0
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2024-05-16 14:45:49 -0400

    Add radius argument for Kuwahara command and fix typo in Image command description
    This commit adds a new argument to adjust the radius for the Kuwahara command in the CLI. Furthermore, a typo in the Image command's description has been corrected, changing 'an fast_image' to 'an image'.

--- crates/cli/src/commands/image/noise/kuwahara.rs
+++ crates/cli/src/commands/image/noise/kuwahara.rs
@@ -1,4 +1,4 @@
-use crate::commands::common::arg::add_input_output_args;
+use crate::commands::common::arg::{add_input_output_args, ArgType};
 use crate::commands::common::command::Command;
 
 pub struct KuwaharaCommand;
@@ -7,7 +7,7 @@ impl Command for KuwaharaCommand {
     fn get() -> clap::Command {
         let cmd = clap::Command::new(Self::name())
             .arg(
-                crate::commands::common::arg::ArgType::Radius.to_arg(),
+                ArgType::Radius.to_arg(),
             )
             .about("Run kuwahara processing pipeline on the image");
         add_input_output_args(cmd)
