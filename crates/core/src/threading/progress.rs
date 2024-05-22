98f71cce1ba092666a13df446b32e893650f9804
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2024-05-22 04:07:36 +0200

    Refactor Progress struct to use InnerProgress
    The Progress struct has been modified to incorporate an InnerProgress struct. This design choice allows for more efficient and safe handling of the contained data. Additional changes include the replacement of Arc with RwLock for the on_increment method allowing for better thread safety.

--- crates/core/src/threading/progress.rs
+++ crates/core/src/threading/progress.rs
@@ -1,12 +1,16 @@
 use std::fmt::Debug;
 use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 #[derive(Clone)]
 pub struct Progress {
-    value: Arc<AtomicUsize>,
-    max_value: Arc<AtomicUsize>,
-    on_increment: Option<Arc<dyn Fn() + Send + Sync>>,
+    inner: Arc<InnerProgress>,
+}
+
+struct InnerProgress {
+    value: AtomicUsize,
+    max_value: AtomicUsize,
+    on_increment: RwLock<Option<Box<dyn Fn() + Send + Sync>>>,
 }
 
 impl Debug for Progress {
@@ -22,36 +26,38 @@ impl Debug for Progress {
 impl Progress {
     pub fn new() -> Self {
         Progress {
-            value: Arc::new(AtomicUsize::new(0)),
-            max_value: Arc::new(AtomicUsize::new(0)),
-            on_increment: None,
+            inner: Arc::new(InnerProgress {
+                value: AtomicUsize::new(0),
+                max_value: AtomicUsize::new(0),
+                on_increment: RwLock::new(None),
+            }),
         }
     }
 
     pub fn setup(&mut self, max_value: usize) {
-        self.max_value.store(max_value, Ordering::SeqCst);
-        self.value.store(0, Ordering::SeqCst);
+        self.inner.max_value.store(max_value, Ordering::SeqCst);
+        self.inner.value.store(0, Ordering::SeqCst);
     }
 
     pub fn increment(&self) {
-        self.value.fetch_add(1, Ordering::SeqCst);
-        if let Some(on_increment) = &self.on_increment {
+        self.inner.value.fetch_add(1, Ordering::SeqCst);
+        if let Some(on_increment) = self.inner.on_increment.read().unwrap().as_ref() {
             on_increment();
         }
     }
 
     pub fn get(&self) -> usize {
-        self.value.load(Ordering::SeqCst)
+        self.inner.value.load(Ordering::SeqCst)
     }
 
     pub fn get_max(&self) -> usize {
-        self.max_value.load(Ordering::SeqCst)
+        self.inner.max_value.load(Ordering::SeqCst)
     }
     pub fn get_percentage(&self) -> f32 {
         (self.get() as f32 / self.get_max() as f32) * 100.0
     }
 
     pub fn set_on_increment<F: Fn() + Send + Sync + 'static>(&mut self, on_increment: F) {
-        self.on_increment = Some(Arc::new(on_increment));
+        *self.inner.on_increment.write().unwrap() = Some(Box::new(on_increment));
     }
 }
