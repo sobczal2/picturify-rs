25863145fd19846dc331bac218298677f3189d96
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2024-06-14 01:19:24 +0200

    Rename test method and update error message
    In the image/empty.rs test file, the name of a function has been updated to more accurately reflect its purpose. The associated error message has also been changed to be more precise, replacing "Missing command" with "Missing subcommand".

--- crates/cli/tests/image/empty.rs
+++ crates/cli/tests/image/empty.rs
@@ -1,12 +1,12 @@
 use crate::common::helpers::get_picturify_cli_cmd;
 
 #[test]
-fn empty_no_args_should_print_missing_command() {
+fn empty_no_args_should_print_missing_subcommand() {
     let mut cmd = get_picturify_cli_cmd();
     cmd.arg("image");
     let assert = cmd.assert();
 
     assert
         .failure()
-        .stderr(predicates::str::contains("Missing command"));
+        .stderr(predicates::str::contains("Missing subcommand"));
 }
